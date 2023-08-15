c0c98508df7219aefec6f57523e9a9046795fb3b
ErnestCrypto <akotobamfo.eab@gmail.com>
2023-08-15 16:17:22 +0000

    commit

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -1,4 +1,7 @@
+$("#loader-cont").hide();
+
 function sendEmail() {
+  $("#loader-cont").show();
   var params = {
     name: document.getElementById("name").value,
     email: document.getElementById("email").value,
@@ -6,24 +9,25 @@ function sendEmail() {
     message: document.getElementById("message").value,
   };
 
-const serviceID = "service_ih3z8k6";
-const templateID = "template_bmj1wqh";
+  const serviceID = "service_ih3z8k6";
+  const templateID = "template_bmj1wqh";
 
-emailjs
-  .send(serviceID, templateID, params)
-  .then((res) => {
-    document.getElementById("name").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("subject").value = "";
-    document.getElementById("message").value = "";
-    console.log(res);
-    alert("Your email has been sent successfully!");
-  })
-  .catch((err) => console.log(err));
-
-
-
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {   
+       $("#loader-cont").hide();
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("subject").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Email has been sent successfully",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    })
+    .catch((err) => console.log(err));
 }
-
-
-
