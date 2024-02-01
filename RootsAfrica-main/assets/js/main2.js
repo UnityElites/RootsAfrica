449190654d1483f36958a96182f1f326db3174dc
ErnestCrypto <akotobamfo.eab@gmail.com>
2024-02-01 13:55:15 +0000

    removing pre-loader, animations and chnging roots to ruuts

---
+++ RootsAfrica-main/assets/js/main2.js
@@ -0,0 +1,32 @@
+
+function sendEmail() {
+document.getElementById("loader-cont").style.display = "flex";
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_ih3z8k6";
+  const templateID = "template_bmj1wqh";
+
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {   
+document.getElementById("loader-cont").style.display = "none";
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("subject").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Email has been sent successfully",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    })
+    .catch((err) => console.log(err));
+}
